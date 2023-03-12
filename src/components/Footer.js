c7bd798063de390855a3784b32af69fdf4e69fd7
Israel Omole <omolewayeolagd1997@gmail.com>
2023-03-12 06:19:45 +0100

    adjusted the images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,10 +14,14 @@ const Footer = () => {
         Address: <span>19, Ogunsola st, Ogba, Lagos</span>{" "}
       </p>
       <div className="flex gap-2">
-        <AiFillTwitterCircle size={25} color="blue" />
-        <AiFillInstagram size={25} color="#ed4511" />
-        <AiFillLinkedin size={25} color="blue" />
-        <AiFillFacebook size={25} color="blue" />
+        <AiFillTwitterCircle
+          size={25}
+          color="blue"
+          className="cursor-pointer"
+        />
+        <AiFillInstagram size={25} color="#ed4511" className="cursor-pointer" />
+        <AiFillLinkedin size={25} color="blue" className="cursor-pointer" />
+        <AiFillFacebook size={25} color="blue" className="cursor-pointer" />
       </div>
     </div>
   );
